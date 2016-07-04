d1193bda988233dc1300f20285602ce26417c008
sjy1203 <shangjunyuan1203@qq.com>
2016-07-04 09:42:13 +0800

    lab4-pre

---
+++ lab4/pi.cu
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda_runtime.h>
+int N;
+int main(int argc,char **argv){
+    int N = 50;
+    if(argc>1) N = atoi(argv[1]);
+    int 
+    return 0;
+}
