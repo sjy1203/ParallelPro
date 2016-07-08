e7bb577cb23f43a9f6584c0b03b254c348773250
Jay <shangjunyuan1203@qq.com>
2016-07-08 09:57:35 +0800

    monte-carol serial&openomp fin

---
+++ project2/pi.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+float pi_cal(int count){
+    int i=0;
+    float x,y;
+    int inCount = 0;
+    for(i=0;i<count;i++){
+        x = rand()/(RAND_MAX+1.0);
+        y = rand()/(RAND_MAX+1.0);
+        if(x*x+y*y<1.0){
+            inCount++;
+        }
+    }
+    return inCount*4.0/count;
+}
+int main(int argc,char **argv){
+    srand((unsigned)time(NULL));
+    int count = 1000;
+    if(argc>1) count = atoi(argv[1]);
+    clock_t start = clock();
+    printf("pi = %.16f,time = %f\n",pi_cal(count),(double)(clock() - start)/1000);
+    return 0;
+}
---
+++ project2/pi_openmp.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+float pi_cal(int count,int thread_num){
+    int i=0;
+    float x,y;
+    int inCount = 0;
+    double start = omp_get_wtime();
+#pragma omp parallel for num_threads(thread_num) reduction(+:inCount)
+    for(i=0;i<count;i++){
+        x = rand()/(RAND_MAX+1.0);
+        y = rand()/(RAND_MAX+1.0);
+        if(x*x+y*y<1.0){
+            inCount = inCount + 1;
+        }
+    }
+    printf("omp time = %f ",omp_get_wtime() - start);
+    return inCount*4.0/count;
+}
+int main(int argc,char **argv){
+    srand((unsigned)time(NULL));
+    int count = 1000,thread_num = 2;
+    if(argc>1) count = atoi(argv[1]);
+    if(argc>2) thread_num = atoi(argv[2]);
+    clock_t start = clock();
+    printf("pi = %.16f,clock() time = %f\n",pi_cal(count,thread_num),(double)(clock() - start)/1000);
+    return 0;
+}
